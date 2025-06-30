a0ac2313efb597f2f3a76d46a09979a233a5f8b1
GovindKrishna <govindkrishna1102@gmail.com>
2025-07-01 01:14:23 +0530

    Bug Fix

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from api.models import User
+from api.models import User, Appointment
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Appointment)
